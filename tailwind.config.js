3acac99e5f954738bf2b2bfb55be35e42cf87822
ikabeee <carlglz30@gmail.com>
2025-06-16 11:43:42 -0500

    chore: update dependencies and add Tailwind CSS configuration
    - Added autoprefixer, postcss, and tailwindcss to devDependencies in package.json
    - Created postcss.config.js to configure PostCSS with Tailwind CSS and autoprefixer
    - Created tailwind.config.js to set up Tailwind CSS with specified content paths

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./app/**/*.{js,tsx,ts,jsx}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
